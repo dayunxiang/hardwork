be01abecc599093fb39ff4a1ff2bc01a95393879
houjingjing <1214992694@qq.com>
2017-10-23 14:22:08 +0800

    workflow

--- workFlow/lib/ZhiShuiMap/webapps/ZhiShuiMap/MapPlatform/js/index.js
+++
@@ -1,9 +0,0 @@
-
-$(".link").text(window.location.origin + "/mapapi/rmap.js");
-
-$(".copy").click(function(){
-	var currentTA = $(".link");
-	currentTA.select(); // 选择对象
-	document.execCommand("Copy"); // 执行浏览器复制命令
-	alert("复制完毕，可粘贴");
-})
